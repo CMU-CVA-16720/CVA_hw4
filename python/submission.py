beb45e42870f05821b63470bd0badf2260e63082
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-10-23 14:46:33 -0400

    WIP Implementing triangulate; output seems reasonable, need more testing

--- python/submission.py
+++ python/submission.py
@@ -5,6 +5,7 @@ Replace 'pass' by your implementation.
 
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
 
 import util
 import helper
@@ -85,8 +86,54 @@ Q3.2: Triangulate a set of 2D coordinates in the image to a set of 3D points.
             err, the reprojection error.
 '''
 def triangulate(C1, pts1, C2, pts2):
-    # Replace pass by your implementation
-    pass
+    N = pts1.shape[0]
+    P = np.zeros((N,3))
+    # Set up matrix A, where Aw=0, and A = (2 * 2 * num_pts) x 4
+    # x1i = C1 @ w1i -> cross(x1i, C1 @ w1i) =0 -> A1i @ wi = 0; two independent equations:
+    # (Cl[3,1]*yl - Cl[2,1])*X + (Cl[3,2]*yl - Cl[2,2])*Y + (Cl[3,3]*yl - Cl[2,3])*Z + (Cl[3,4]*yl - Cl[2,4]) = 0
+    # (Cl[1,1] - Cl[3,1]*xl)*X + (Cl[1,2] - Cl[3,2]*xl)*Y + (Cl[1,3] - Cl[3,3]*xl)*Z + (Cl[1,4] - Cl[3,4]*xl) = 0
+    # (Cr[3,1]*yr - Cr[2,1])*X + (Cr[3,2]*yr - Cr[2,2])*Y + (Cr[3,3]*yr - Cr[2,3])*Z + (Cr[3,4]*yr - Cr[2,4]) = 0
+    # (Cr[1,1] - Cr[3,1]*xr)*X + (Cr[1,2] - Cr[3,2]*xr)*Y + (Cr[1,3] - Cr[3,3]*xr)*Z + (Cr[1,4] - Cr[3,4]*xr) = 0
+    Cl = C1
+    Cr = C2
+    for i in range(0,N):
+        # Get coordinates
+        xl, yl = pts1[i,:]
+        xr, yr = pts2[i,:]
+        print('Coord: ({},{}), ({},{})'.format(xl,yl,xr,yr))
+        # Compute A
+        A = np.zeros((4,4))
+        A[0,:] = np.array([Cl[2,0]*yl - Cl[1,0], Cl[2,1]*yl - Cl[1,1], Cl[2,2]*yl - Cl[1,2], Cl[2,3]*yl - Cl[1,3]])
+        A[1,:] = np.array([Cl[0,0] - Cl[2,0]*xl, Cl[0,1] - Cl[2,1]*xl, Cl[0,2] - Cl[2,2]*xl, Cl[0,3] - Cl[2,3]*xl])
+        A[2,:] = np.array([Cr[2,0]*yr - Cr[1,0], Cr[2,1]*yr - Cr[1,1], Cr[2,2]*yr - Cr[1,2], Cr[2,3]*yr - Cr[1,3]])
+        A[3,:] = np.array([Cr[0,0] - Cr[2,0]*xr, Cr[0,1] - Cr[2,1]*xr, Cr[0,2] - Cr[2,2]*xr, Cr[0,3] - Cr[2,3]*xr])
+        # Compute w
+        u, s, vT = np.linalg.svd(A)
+        print('SVD:\n\tu=\n{}\n\ts=\n{}\n\tvT=\n{}'.format(u,s,vT))
+        w = vT[-1]
+        # Convert to non-homogeneous coord, then save
+        w /= w[-1]
+        P[i,:] = w[0:3]
+    # Compute error
+    err = 0
+    for i in range(0,N):
+        # Get x1 and x2
+        x1 = pts1[i,:]
+        x2 = pts1[i,:]
+        # Compute x1_hat, x2_hat
+        x1_hat = C1@np.append(P[i,:],1)
+        x1_hat /= x1_hat[-1]
+        x1_hat = x1_hat[0:2]
+        x2_hat = C2@np.append(P[i,:],1)
+        x2_hat /= x2_hat[-1]
+        x2_hat = x2_hat[0:2]
+        # Update error
+        err += np.sum(np.square(x1-x1_hat))
+        err += np.sum(np.square(x2-x2_hat))
+        # Debug
+        print('x1, x1_hat: ({}), ({})'.format(x1, x1_hat))
+        print('x2, x2_hat: ({}), ({})'.format(x2, x2_hat))
+    return P, err
 
 
 '''
@@ -222,7 +269,11 @@ if __name__ == "__main__":
     C1 = K1 @ M1
     C2 = K2 @ M2
     #[w, err] = triangulate(C1, pts1, C2, pts2)
-    triangulate(C1, pts1, C2, pts2)
+    [P, err] = triangulate(C1, pts1, C2, pts2)
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(P[:,0], P[:,1], P[:,2])
+    plt.show()
     pass
     
 
