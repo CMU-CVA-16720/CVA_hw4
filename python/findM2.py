37967c02cc532f85b5b774afc529f3017860e965
Jae_PC <jaekyuns@andrew.cmu.edu>
2021-11-06 11:33:34 -0400

    Added findM2_EC for Q5.3

--- python/findM2.py
+++ python/findM2.py
@@ -45,5 +45,26 @@ def findM2():
        P=P
     )
 
+def findM2_EC(pts1, pts2, K1, K2, E):
+    # Compute M2 possibilities
+    M2s = helper.camera2(E)
+    # Compute 3D coordinates using all three possibilities
+    M1 = np.zeros((3,4))
+    M1[:,0:3] = np.identity(3)
+    C1 = K1 @ M1
+    P_array = np.zeros((pts1.shape[0],3*4))
+    for i in range(0,4):
+        C2 = K2 @ M2s[:,:,i]
+        [P, err] = submission.triangulate(C1, pts1, C2, pts2)
+        P_array[:,3*i:3*i+3] = P
+    # Correct M2 should have all positive Z values, since object is in front of cameras
+    # Therefore pick M2 with most positive Z values
+    M2_indx = np.argmax(np.count_nonzero(P_array[:,2::3]>0,axis=0))
+    M2 = M2s[:,:,M2_indx]
+    C2 = K2 @ M2s[:,:,M2_indx]
+    P = P_array[:,M2_indx*i:3*M2_indx+3]
+    # Return M2
+    return M2, C2, P
+
 if __name__ == "__main__":
     findM2()
